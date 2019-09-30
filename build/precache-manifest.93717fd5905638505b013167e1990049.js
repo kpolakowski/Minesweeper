2fd65b200db25688b6123542a7c66c2fb7d11ad4
pulamc <karpol1245@gmail.com>
2019-09-30 15:52:30 +0200

    Created production build

---
+++ build/precache-manifest.93717fd5905638505b013167e1990049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3c9b2f2a89e4bfae6a6fa4912dcab5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8796f6fb56708cb2b2be",
+    "url": "/static/css/main.adebad43.chunk.css"
+  },
+  {
+    "revision": "f4a1929f389cb08a5cd6",
+    "url": "/static/js/2.137c3002.chunk.js"
+  },
+  {
+    "revision": "8796f6fb56708cb2b2be",
+    "url": "/static/js/main.3c8d56d9.chunk.js"
+  },
+  {
+    "revision": "31e45f4b9d8dda1b257a",
+    "url": "/static/js/runtime~main.01516ae6.js"
+  },
+  {
+    "revision": "ce968140cd56ee77df69528c505739c4",
+    "url": "/static/media/bomb.ce968140.svg"
+  }
+]);
